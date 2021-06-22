dc7b795ca97d582904f69ee543db106c56717323
Filipe Lacerda <filipeem.web@gmail.com>
2021-06-22 11:38:40 -0300

    Cria os components funcionais e adiciona estilo com o Material UI

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
+
+function FormularioCadastro() {
+  let nome = "";
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(nome);
+      }}
+    >
+      <TextField
+        value={nome}
+        onChange={(event) => {
+          nome = event.target.value;
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+
+      <TextField
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch defaultChecked name="Promoções" color="primary" />}
+      />
+
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch defaultChecked name="Novidades" color="primary" />}
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+
+export default FormularioCadastro;
